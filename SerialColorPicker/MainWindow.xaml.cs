d44a4eadd755f989439f96ebc189bf8272f1d6e5
Christian Conkle <christian@conkle.org>
2018-11-25 22:52:37 -0800

    Initial commit

--- SerialColorPicker/MainWindow.xaml.cs
+++ SerialColorPicker/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+using System.IO.Ports;
+using System.Threading;
+using System.Reactive.Linq;
+
 namespace SerialColorPicker
 {
     /// <summary>
@@ -20,9 +25,39 @@ namespace SerialColorPicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        static SerialPort _serialPort;
+
         public MainWindow()
         {
             InitializeComponent();
+            _serialPort = new SerialPort("COM3", 115200);
+
+            _serialPort.Open();
+
+            Observable
+                .FromEventPattern<RoutedPropertyChangedEventArgs<Color?>>(ClrPcker_Background, "SelectedColorChanged")
+                .Sample(TimeSpan.FromSeconds(0.1))
+                .ObserveOnDispatcher()
+                .Subscribe(x => HandleColor(x.EventArgs.NewValue.Value));
+            
+
+        }
+
+        void HandleColor(Color e)
+        {
+            String s = String.Format("{0},{1},{2},{3}",
+                ClrPcker_Background.SelectedColor.Value.R.ToString(),
+                ClrPcker_Background.SelectedColor.Value.G.ToString(),
+                ClrPcker_Background.SelectedColor.Value.B.ToString(),
+                ClrPcker_Background.SelectedColor.Value.A.ToString());
+            _serialPort.WriteLine(s);
+            Console.WriteLine(s);
+            //MessageBox.Show("#" + ClrPcker_Background.SelectedColor.Value.R.ToString() + ClrPcker_Background.SelectedColor.Value.G.ToString() + ClrPcker_Background.SelectedColor.Value.B.ToString());
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _serialPort.Close();
         }
     }
 }
